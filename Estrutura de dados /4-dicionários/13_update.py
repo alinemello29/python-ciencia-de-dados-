ed38d3f61ae8ac3039c9040473fb91dd36aa33c6
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-09-22 22:15:32 -0300

    Update 13_update.py

--- Estrutura de dados /4-dicionários/13_update.py
+++ Estrutura de dados /4-dicionários/13_update.py
@@ -1,8 +1,8 @@
-contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+contatos = {"alinne@gmail.com": {"nome": "aline", "telefone": "3333-2221"}}
 
-contatos.update({"guilherme@gmail.com": {"nome": "Gui"}})
-print(contatos)  # {'guilherme@gmail.com': {'nome': 'Gui'}}
+contatos.update({"alinne@gmail.com": {"nome": "line"}})
+print(contatos)  # {'alinne@gmail.com': {'nome': 'line'}}
 
-contatos.update({"giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3322-8181"}})
-# {'guilherme@gmail.com': {'nome': 'Gui'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3322-8181'}}
+contatos.update({"mello@gmail.com": {"nome": "melo", "telefone": "3322-8181"}})
+# {'alinne@gmail.com': {'nome': 'line'}, 'mello@gmail.com': {'nome': 'melo', 'telefone': '3322-8181'}}
 print(contatos)
