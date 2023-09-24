c932d3f7e47b0b1431e7a17621469de970d0fd38
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-09-24 11:46:39 -0400

    Update 00_declarando_dicionarios.py

--- Estrutura de dados /4-dicionários/00_declarando_dicionarios.py
+++ Estrutura de dados /4-dicionários/00_declarando_dicionarios.py
@@ -1,8 +1,8 @@
-pessoa = {"nome": "Guilherme", "idade": 28}
+pessoa = {"nome": "aline", "idade": 26}
 print(pessoa)
 
-pessoa = dict(nome="Guilherme", idade=28)
+pessoa = dict(nome="aline", idade=26)
 print(pessoa)
 
-pessoa["telefone"] = "3333-1234"  # {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+pessoa["telefone"] = "3333-1234"  # {"nome": "aline", "idade": 26, "telefone": "3333-1234"}
 print(pessoa)
