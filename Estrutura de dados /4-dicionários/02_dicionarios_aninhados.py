e891ba8d6ead49a992c98b21696ac2bf96621f21
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-09-24 11:51:12 -0400

    Update 02_dicionarios_aninhados.py

--- Estrutura de dados /4-dicionários/02_dicionarios_aninhados.py
+++ Estrutura de dados /4-dicionários/02_dicionarios_aninhados.py
@@ -2,7 +2,7 @@ contatos = {
     "aline@gmail.com": {"nome": "aline", "telefone": "3333-2221"},
     "ana@gmail.com": {"nome": "ana", "telefone": "3443-2121"},
     "melo@gmail.com": {"nome": "melo", "telefone": "3344-9871"},
-    "maria@gmail.com": {"nome": "Maria", "telefone": "3333-7766"},
+    "maria@gmail.com": {"nome": "maria", "telefone": "3333-7766"},
 }
 
 telefone = contatos["aline@gmail.com"]["telefone"]  # "3443-2121"
