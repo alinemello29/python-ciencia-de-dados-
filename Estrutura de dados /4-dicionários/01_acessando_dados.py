ef5db01fe0552f32c55f2ed677824e9956f45a82
Aline Mello <109696840+alinemello29@users.noreply.github.com>
2023-09-24 11:47:46 -0400

    Update 01_acessando_dados.py

--- Estrutura de dados /4-dicionários/01_acessando_dados.py
+++ Estrutura de dados /4-dicionários/01_acessando_dados.py
@@ -1,11 +1,11 @@
-dados = {"nome": "Guilherme", "idade": 28, "telefone": "3333-1234"}
+dados = {"nome": "aline", "idade": 26, "telefone": "3333-1234"}
 
-print(dados["nome"])  # "Guilherme"
-print(dados["idade"])  # 28
+print(dados["nome"])  # "aline"
+print(dados["idade"])  # 26
 print(dados["telefone"])  # "3333-1234"
 
-dados["nome"] = "Maria"
+dados["nome"] = "Melo"
 dados["idade"] = 18
 dados["telefone"] = "9988-1781"
 
-print(dados)  # {"nome": "Maria", "idade": 18, "telefone": "9988-1781"}
+print(dados)  # {"nome": "Melo", "idade": 18, "telefone": "9988-1781"}
